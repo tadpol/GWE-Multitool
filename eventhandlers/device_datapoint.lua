8e52c0f4471b3b8445228fb3a75242f54990a332
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-09-27 08:16:54 -0500

    Sort all the data coming in and store what we want

--- eventhandlers/device_datapoint.lua
+++ eventhandlers/device_datapoint.lua
@@ -1,10 +1,22 @@
 --#EVENT device datapoint
 
+-- Get the timestamp for this data.
+local stamped = nil
+if data.api == "record" then
+  stamped = tostring(data.value[1]) .. '000000000'
+end
+
+-- Keep a set of devices (aka serial numbers)
+Keystore.command{
+  key='serialNumbers',
+  command = 'sadd',
+  args = { data.device_sn }
+}
+
 -- If the alias is in [device_info, usage_report, update_interval, engine_report,
 -- engine_fetch] don't write to TS.
 -- Instead append them to the KV logs.
 local gwe_fields = ['device_info', 'usage_report', 'update_interval', 'engine_report', 'engine_fetch']
-
 if table.contains(gwe_fields, data.alias) then
   Keystore.command{
     key = data.alias .. "." .. data.device_sn,
@@ -16,18 +28,45 @@ if table.contains(gwe_fields, data.alias) then
     command = 'ltrim',
     args = { 0, 20 }
   }
-else
 
-  -- Write Device Resource Data to timeseries database
-  local stamped = nil
-  if data.api == "record" then
-    stamped = tostring(data.value[1]) .. '000000000'
+  -- Now look at sensors_report,
+elseif data.alias == "sensors_report" then
+  local values, err = from_json(data.value[2])
+
+  if values ~= nil then
+    local tags = {gwe = data.device_sn, sn = data.device_sn}
+    --  if there is a sn field, add that serial number too.
+    if values.sn ~= nil then
+      Keystore.command{
+        key='serialNumbers',
+        command = 'sadd',
+        args = { value.sn }
+      }
+      tags.sn = value.sn
+      value.sn = nil
+    end
+
+    -- only numbers.
+    local fields = {}
+    for k,v in pairs(value) do
+      if type(v) == 'number' then
+        fields[k] = v
+      end
+    end
+
+    Timeseries.write{
+      query = TSW.write('data', tags, fields, stamped)
+    }
   end
 
-  -- Coming from GWE, we don't want to just store some of these.
-  Timeseries.write{
-    query = TSW.write('data', {identifier=data.device_sn}, {[data.alias] = data.value[2]}, stamped)
-  }
+else
+  -- Some new alias; assume it goes in TSDB.
+  -- well, if it is a number.
+  if type(data.value[2]) == 'number' then
+    Timeseries.write{
+      query = TSW.write('data', {sn=data.device_sn}, {[data.alias] = data.value[2]}, stamped)
+    }
+  end
 end
 
 -- vim: set et ai sw=2 ts=2 :
