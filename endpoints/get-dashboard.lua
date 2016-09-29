d87884d274b319caa7a2f3b55dd533c8815a0d25
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-09-29 10:46:47 -0500

    Inject datasources for the existing serial numbers.

--- endpoints/get-dashboard.lua
+++ endpoints/get-dashboard.lua
@@ -1,21 +1,71 @@
 --#ENDPOINT GET /v1/dashboard
 
+function injectDatasources(datasources)
+	local ret = Keystore.get{key='serialNumbers'}
+	if ret.value == nil then
+		return datasources
+	end
+
+	for _, sn in ipairs(ret.value) do
+		local name = 'GWE Info ' .. sn
+		local idx, ads = table.find(datasources, 'name', name)
+		if idx == nil then
+			ads = {
+				name = 'GWE Info ' .. sn,
+				type = 'JSON',
+				settings = {
+					method = 'GET',
+					refresh = 10,
+					url = '/v1/report/' .. sn,
+					use_thingproxy = false
+				}
+			}
+			datasources[#datasources + 1] = ads
+		end
+
+		name = 'Data ' .. sn
+		idx, ads = table.find(datasources, 'name', name)
+		if idx == nil then
+			ads = {
+				name = 'Data ' .. sn,
+				type = 'JSON',
+				settings = {
+					method = 'GET',
+					refresh = 10,
+					url = '/v1/data/' .. sn,
+					use_thingproxy = false
+				}
+			}
+			datasources[#datasources + 1] = ads
+		end
+	end
+
+	return datasources
+end
+
+
 local got = Keystore.get{key='dashboard.0'}
 if got.code ~= nil then
 	response.code = got.code
 	response.message = got
 elseif got.value == nil then
-	response.code = 500
-	response.message = "No value, no error."
+	response.message = {
+		allow_edit = true,
+		columns = 3,
+		datasources = injectDatasources({}),
+		version = 1
+	}
 else
 	local ex, err = from_json(got.value)
 	if ex ~= nil then
+
+		ex.datasources = injectDatasources(ex.datasources)
+
 		response.message = ex
 	else
 		response.code = 500
 		response.message = err
 	end
 end
-
 
 -- vim: set ai sw=2 ts=2 :
