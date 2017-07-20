2802753adca37988aae0a40e89e70ae07eab6e7d
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-07-20 09:37:03 -0500

    An untested firstpass at an ADC data_in handler

---
+++ eventhandlers/device2_event.lua
@@ -0,0 +1,76 @@
+--xEVENT device datapoint
+-- luacheck: globals event (magic variable from Murano)
+
+-- Keep a set of devices (aka serial numbers)
+Keystore.command{
+  key='serialNumbers',
+  command = 'sadd',
+  args = { event.identity }
+}
+
+if event['type'] ~= 'data_in' then
+	return
+end
+
+--
+-- [ {timestamp=<ts>, values = [ {<alias>= value}, … ] }, … ]
+
+-- All of this below is trying to be proactivily smart about grabbing who-knows-what
+-- and saving it to TSDB.
+--
+
+for _, tsval in ipairs(event.payload) do
+	local ts = tsval.timestamp
+	for _, aliasValue in ipairs(tsval.values) do
+		for alias, value in pairs(aliasValue) do
+			if table.contains(GWE.Fields, alias) then
+				local key = string.gsub(alias .. "." .. event.identity, '[^%w@.-]', '-')
+				Keystore.command{
+					key = key,
+					command = 'lpush',
+					args = { value }
+				}
+				Keystore.command{
+					key = key,
+					command = 'ltrim',
+					args = { 0, 20 }
+				}
+				-- 
+			else
+				local toWrite = {
+					tags = { sn = event.identity },
+					metrics = {},
+					ts = ts
+				}
+				if type(value) == 'number' then
+					toWrite.metrics[alias] = value
+				else
+					local jvals, err = from_json(value)
+					if err ~= nil then
+						toWrite.metrics[alias] = value
+					else
+						toWrite.tags.gwe = event.identity
+						--  if there is a sn field, add that serial number too.
+						if jvals.sn ~= nil then
+							toWrite.tags.sn = jvals.sn
+							Keystore.command{
+								key='serialNumbers',
+								command = 'sadd',
+								args = { jvals.sn }
+							}
+						end
+						-- only numbers.
+						for k,v in pairs(jvals) do
+							if type(v) == 'number' then
+								toWrite.metrics[k] = v
+							end
+						end
+					end
+				end
+				Tsdb.write(toWrite)
+			end
+		end
+	end
+end
+
+-- vim: set ai sw=2 ts=2 :
