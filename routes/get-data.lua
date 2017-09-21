5f04b13c450d65ee1c027a7d135655247d5dd700
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-09-21 15:34:08 -0500

    Try reshaping the TSDB output to something more friendly to freeboard.

--- routes/get-data.lua
+++ routes/get-data.lua
@@ -8,20 +8,37 @@ local identifier = request.parameters.sn
 local metrics
 local key = string.gsub("_metric_names." .. identifier, '[^%w@.-]', '-')
 metrics = Keystore.get{key=key}
-if metrics.error ~= nil then
+if metrics.value == nil or #metrics.value == 0 then
   metrics = Tsdb.listMetrics()
   metrics = metrics.metrics
 else
   metrics = metrics.value
 end
 
+-- TODO: Allow pulling in of some of the arguments to TSDB?
 local out = Tsdb.query{
   tags={sn=identifier},
   metrics=metrics,
   limit = 100,
   epoch = 'ms',
+  fill = 'null',
 }
 
-return out
+if request.parameters.raw ~= nil then
+  return out
+else
+
+  -- As Array of Objects.
+  local resulting = setmetatable({}, {['__type']='slice'})
+  for _, row in ipairs(out.values) do
+    local nrow = {}
+    for cidx,cname in ipairs(out.columns) do
+      nrow[cname] = row[cidx]
+    end
+    table.insert(resulting, nrow)
+  end
+
+  return resulting
+end
 
 -- vim: set et ai sw=2 ts=2 :
